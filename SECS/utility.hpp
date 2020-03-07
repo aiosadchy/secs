8651615dc97f64f2273ecc86cfbb344bd657bab9
Alexander Osadchy <a.i.osadchy@gmail.com>
2020-03-08 02:04:24 +0300

    Remove type_utils

--- SECS/utility.hpp
+++ SECS/utility.hpp
@@ -5,7 +5,6 @@
 #include "utility/memory.hpp"
 #include "utility/range.hpp"
 #include "utility/repeat.hpp"
-#include "utility/type_utils.hpp"
 #include "utility/types.hpp"
 
 #endif // SECS_UTILITY_HPP
